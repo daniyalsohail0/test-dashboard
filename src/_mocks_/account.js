01c239181a2fe8b70b9b3ab49d73db2fd3a7b670
daniyalsohail0 <daniyalsohail0@gmail.com>
2022-07-14 19:01:11 +0500

    first commit

---
+++ src/_mocks_/account.js
@@ -0,0 +1,9 @@
+// ----------------------------------------------------------------------
+
+const account = {
+  displayName: 'Daniyal Sohail',
+  email: 'daniyalsohail0@gmail.com',
+  photoURL: '/static/mock-images/avatars/avatar_default.jpg'
+};
+
+export default account;
